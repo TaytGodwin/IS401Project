7c53f8ac2c662f60d2731fa3202c12660fe9b4ee
taterg99 <taterg99@gmail.com>
2025-03-12 10:11:35 -0600

    Deleted stuff not needed

--- backend/IS401Project/IS401Project/Data/NotableDbContext.cs
+++ backend/IS401Project/IS401Project/Data/NotableDbContext.cs
@@ -12,23 +12,5 @@ namespace IS401Project.Data
         public DbSet<Note> Notes { get; set; }
         public DbSet<TagNote> TagNotes { get; set; } // Linking table
 
-        // This is an overriding function to make a linking table when the database is created
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<TagNote>()
-                .HasKey(tn => new { tn.NoteId, tn.TagId });  // Tells system that TagNote has a composite key
-
-            // Shows relationship between TagNote and Note table (many to one)
-            modelBuilder.Entity<TagNote>()
-                .HasOne(tn => tn.Note)
-                .WithMany(n => n.TagNotes)
-                .HasForeignKey(tn => tn.NoteId);
-
-            // Shows relationship between TagNote and Tag table (many to one)
-            modelBuilder.Entity<TagNote>()
-                .HasOne(tn => tn.Tag)
-                .WithMany(t => t.TagNotes)
-                .HasForeignKey(tn => tn.TagId);
-        }
     }
 }
