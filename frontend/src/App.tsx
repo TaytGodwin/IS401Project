a52df0f13073ef750c949d1c5a3247c129967a05
IcyThunder22 <153141281+IcyThunder22@users.noreply.github.com>
2025-03-17 11:07:23 -0600

    add notes app to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import LoginPage from './components/Login/LoginPage';
 import StudyDashboard from './components/Home/StudyDashboard';
 import NotableApp from './components/CreatePost/NotableApp';
 import NotesApp from './components/Notes Page/NotesApp';
 import './App.css';
 
-import Layout from './components/Layout/Layout'
+import Layout from './components/Layout/Layout';
 import React from 'react';
 import PublicGroupsPage from './components/Groups';
 
 function App() {
   return (
     <>
-    <Layout>
-      <LoginPage />;
-      <StudyDashboard />;
-      <PublicGroupsPage />;
-    </Layout>
-      
+      <Layout>
+        <LoginPage />;
+        <StudyDashboard />;
+        <PublicGroupsPage />;
+        <NotesApp />
+      </Layout>
     </>
   );
 }
