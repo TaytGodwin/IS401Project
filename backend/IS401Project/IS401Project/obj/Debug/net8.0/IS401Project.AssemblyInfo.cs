87d109728ed7a18f27ae50b53338bb9009fd5d80
Mekeli Hiller <mekelihiller@gmail.com>
2025-03-31 08:34:53 -0600

    This

--- backend/IS401Project/IS401Project/obj/Debug/net8.0/IS401Project.AssemblyInfo.cs
+++ backend/IS401Project/IS401Project/obj/Debug/net8.0/IS401Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IS401Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4754294afbc65049c30066de48d4948bb5f3fb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590b62d3c08d99a947851e135965cc604b676028")]
 [assembly: System.Reflection.AssemblyProductAttribute("IS401Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IS401Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
